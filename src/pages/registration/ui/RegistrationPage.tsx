04da4d5b9471497b2f70b2fa9e894aedeceec840
Matesero <grishenkov05@mail.ru>
2025-03-13 10:09:37 +0700

    remove layout reg page

--- src/pages/registration/ui/RegistrationPage.tsx
+++ src/pages/registration/ui/RegistrationPage.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import { Layout } from '~/app/layout';
 import { authorizationFeature } from '~/features';
 
 const { RegistrationForm } = authorizationFeature.ui;
 
 export const RegistrationPage = () => {
     return (
-        <Layout>
-            <RegistrationForm />
-        </Layout>
+        <RegistrationForm />
     );
 };
